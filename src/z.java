718eca6800c589c04014858808b6681ed7597645
Ryan Burke <ryanburketv@gmail.com>
2020-05-16 00:50:42 -0400

    building class structure

---
+++ src/z.java
@@ -0,0 +1,8 @@
+public class z {
+
+    public static void main(String[] args) {
+        
+        String h = FilenameUtils.getExtension("/path/to/file/foo.txt");
+        System.out.print(h);
+    }
+}
