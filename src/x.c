b790c7056b1e2458cf5804063bfd979412a1a986
Ryan Burke <ryanburketv@gmail.com>
2020-05-19 14:07:13 -0400

    messing around with passing pointers to functions to change them

--- src/x.c
+++ src/x.c
@@ -1,18 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+
 
 #include "fmmap.h"
+
+void test(char **f);
 
 int main() {
 
     FM *fm = malloc(sizeof(FM));
+    fm->F = malloc(6);
+    strcpy(fm->F, "hello");
+    // printf("%s\n", fm->F);
     test(&(fm->F));
+    // printf("%s\n", fm->F);
+    free(fm->F);
+    free(fm); // why not free?
+
+    return 0;
 
 }
 
-void test(**f) {
-    
+void test(char **F) {
+    *F = "ryan";
 }
