57c83f6dc9fc6bb4b392bec78325ef532992407a
Ryan Burke <ryanburketv@gmail.com>
2020-11-08 01:40:13 -0500

    Update build.c

--- src/build.c
+++ src/build.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
     char *reads = argv[3];
     char *alignOut = argv[4];
 
-    /* [./fmmap default] executes with small default inputs */
+    /* [./run] executes with small default inputs */
     if (argv[1] == NULL) {
         ref_seq = "./default/ref-small.fa";
         indexOut = "./fm-output/FMindex.txt";
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
         makeProgressBar = true;
     }
 
-    /* [./fmmap covid] executes for coronavirus genome with 1,000 reads*/
+    /* [./run covid 1k] executes for coronavirus genome with 1,000 reads*/
     else if (strcmp(lower(argv[1]), "covid") == 0 && (strcmp(argv[2], "1K") == 0 || strcmp(argv[2], "1k") == 0)) {
         ref_seq = "2019-nCoV.fa";
         indexOut = "./fm-output/FMindex.txt";
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
         makeProgressBar = true;
     }
 
-    /* [./fmmap covid] executes for coronavirus genome with 10,000 reads*/
+    /* [./run covid 10k] executes for coronavirus genome with 10,000 reads*/
     else if (strcmp(lower(argv[1]), "covid") == 0 && (strcmp(argv[2], "10K") == 0 || strcmp(argv[2], "10k") == 0)) {
         ref_seq = "2019-nCoV.fa";
         indexOut = "./fm-output/FMindex.txt";
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
         makeProgressBar = true;
     }
 
-    /* [./fmmap covid 1M] executes for coronavirus genome with 1 Million reads */
+    /* [./run covid 1M] executes for coronavirus genome with 1 Million reads */
     else if (strcmp(lower(argv[1]), "covid") == 0 && (strcmp(argv[2], "1M") == 0 || strcmp(argv[2], "1m") == 0)) {
         ref_seq = "2019-nCoV.fa";
         indexOut = "./fm-output/FMindex.txt";
@@ -76,6 +76,18 @@ int main(int argc, char **argv) {
 
         /* pass # of reads and make progress bar */
         READ = 1000000;
+        makeProgressBar = true;
+    } 
+    
+    /* just execute default*/
+    else {
+        ref_seq = "./default/ref-small.fa";
+        indexOut = "./fm-output/FMindex.txt";
+        reads = "./default/reads-small.fa";
+        alignOut = "./mappings/mapping.sam";
+
+        /* pass # of reads and make progress bar */
+        READ = 1;
         makeProgressBar = true;
     }
 
